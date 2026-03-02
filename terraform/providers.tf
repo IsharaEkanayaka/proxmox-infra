753f6acf819345ec64ab228d4b9896e02993d429
Chanuth412-pixel <e21007@eng.pdn.ac.lk>
2026-03-02 21:48:32 +0530

    check 1 memory

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,8 @@
+terraform {
+  backend "local" {
+    path = "C:/terraform-state/terraform.tfstate"
+  }
+}
 provider "proxmox" {
   endpoint = var.proxmox_api_url
   api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
